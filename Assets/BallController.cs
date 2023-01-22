25a69cf57584671c6f39ea2f8287bbed00d5e20e
hirororohirotsugu <hirororo16312@gmail.com>
2023-01-22 15:36:30 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最小値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバーを表示するテキスト
+    private GameObject gameoverText;
+
+    //得点を表示するテキスト
+    private GameObject tokutenText;
+
+    //得点
+    public int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーンの中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //上に倣いシーンのTokutenTextオブジェクト取得
+        this.tokutenText = GameObject.Find("TokutenText");
+
+        //TokutenTextにscoreを表示
+        this.tokutenText.GetComponent<Text>().text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if(this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバーを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+        //TokutenTextにscoreを表示
+        this.tokutenText.GetComponent<Text>().text = score.ToString();
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 10;;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.score += 30;
+        }
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //はじいた時の傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印キー、Ａキーを押したとき左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag" || Input.GetKeyDown(KeyCode.A) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印キー、Ｄキーを押したとき右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag" || Input.GetKeyDown(KeyCode.D) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //下矢印キー、Ｓキーを押したとき両フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.DownArrow) && tag == "LeftFripperTag" || Input.GetKeyDown(KeyCode.S) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && tag == "RightFripperTag" || Input.GetKeyDown(KeyCode.S) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+
+        //矢印キー、Ａ、Ｓ、Ｄキーが離されたときフリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow)&& tag == "LeftFripperTag" || Input.GetKeyUp(KeyCode.A) && tag == "LeftFripperTag" || Input.GetKeyUp(KeyCode.DownArrow) && tag == "LeftFripperTag" || Input.GetKeyUp(KeyCode.S) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow)&& tag == "RightFripperTag" || Input.GetKeyUp(KeyCode.D) && tag == "RightFripperTag" || Input.GetKeyUp(KeyCode.DownArrow) && tag == "RightFripperTag" || Input.GetKeyUp(KeyCode.S) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
